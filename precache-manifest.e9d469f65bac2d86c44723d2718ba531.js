4c4a60ff61bb141531ddf65e3bf2cedd063fa61d
Daniel Park <ekklesia89@gmail.com>
2019-10-25 00:10:11 +0900

    Updates

---
+++ precache-manifest.e9d469f65bac2d86c44723d2718ba531.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a22be93be7413034fd03f14dbb30cf3d",
+    "url": "/chanhyun-portfolio/index.html"
+  },
+  {
+    "revision": "709f26bea6aaa5840404",
+    "url": "/chanhyun-portfolio/static/css/main.5c7da384.chunk.css"
+  },
+  {
+    "revision": "3822bcac2c946e3988e3",
+    "url": "/chanhyun-portfolio/static/js/2.35f792f2.chunk.js"
+  },
+  {
+    "revision": "709f26bea6aaa5840404",
+    "url": "/chanhyun-portfolio/static/js/main.d48b8435.chunk.js"
+  },
+  {
+    "revision": "a82edd9eb7a40677cd2d",
+    "url": "/chanhyun-portfolio/static/js/runtime-main.46efd609.js"
+  },
+  {
+    "revision": "9d903a97a6a4d20fdf6b5e4c7906e629",
+    "url": "/chanhyun-portfolio/static/media/daniel_resume.9d903a97.pdf"
+  },
+  {
+    "revision": "ee54d9d5d00555ccbce392e376c6db42",
+    "url": "/chanhyun-portfolio/static/media/profile.ee54d9d5.jpg"
+  },
+  {
+    "revision": "c8063b3777ac283438da55949a606a87",
+    "url": "/chanhyun-portfolio/static/media/project1.c8063b37.png"
+  },
+  {
+    "revision": "3bf2fabd98e3e2d733df1e5540817d99",
+    "url": "/chanhyun-portfolio/static/media/project2.3bf2fabd.png"
+  },
+  {
+    "revision": "93d640face969100571d6a4ee7e4b803",
+    "url": "/chanhyun-portfolio/static/media/secondhand.93d640fa.png"
+  }
+]);
